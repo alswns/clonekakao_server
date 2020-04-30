669b5ba8011d874e2df59958d2e16665a0d22819
alswns <49472611+alswns@users.noreply.github.com>
2020-05-01 04:15:05 +0900

    test

--- app.py
+++ app.py
@@ -26,7 +26,13 @@ def isRegi():
         return "true"
     return "false"
 
-
+@app.route('/getImage')
+def getImage():
+    auth=db['auth']
+    data=auth.find({"phone":"0104942613"})
+    for i in data:
+        print(i['image'])
+        return i['image']
 @app.route('/register',methods=['POST'])
 def regiser():
     auth=db['auth']
