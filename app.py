d8b4f6bec9e72e14af47424f2772016ad5ded62d
alswns <49472611+alswns@users.noreply.github.com>
2020-04-30 15:12:48 +0900

    login

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,abort
 import pymongo
 from pymongo import MongoClient
-import json
+import re
+
 conn=MongoClient('localhost',27017)
 db=conn['auth']
 app = Flask(__name__)
@@ -12,9 +13,36 @@ app = Flask(__name__)
 def index():
     return 'Hello Flask'
     
-@app.route('/info')
-def info():
-    return 'Info'
+@app.route('/isregi')
+def isRegi():
+    auth=db['auth']
+    phone=request.args.get('phone')
+    # regex = re.compile(r'(\d{3})-(\d{4}-\d{4})')
+    # matchobj=regex.search(phone)
+    # areaCode=matchobj.group(1)
+    # num=matchobj.group(2)
+    if phone==None:
+        abort(400)
+        return False
+    return True
+
+@app.route('/register',methods=['POST'])
+def regiser():
+    auth=db['auth']
+    json=request.json
+    phone=json['phone']
+    for i in auth.find({'phone':phone}):
+        print('이미있는아이디입니다')
+        return "이미있는아이디"
+    if json['pw']!=json['repw']:
+        print('비밀번호가 일치하지않습니다')
+        return "비밀번호가 일치하지않음"
+    auth.insert(json)
+    print('회원가입성공')
+    return "회원가입 성공"
+    print(json)
+    
+    # auth.insert()
 @app.route('/auth',methods=['POST'])
 def login():
     auth=db['auth']
@@ -22,16 +50,17 @@ def login():
     
     user=request.get_json()
     json=request.json
-    id=json['id']
+    print(json)
+    phone=json['id']
     pw=json['pw']
     a=auth.find({'id':id})
     for i in a:
         print(i)
         if i['pw']==pw:
-            return '일치'
+            return 'true'
 
     if id==None or pw==None:
         return None
-    return '불일치'
+    return 'false'
 
-app.run(port=80, debug=True) 
+app.run(host='0.0.0.0',port=80, debug=True) 
